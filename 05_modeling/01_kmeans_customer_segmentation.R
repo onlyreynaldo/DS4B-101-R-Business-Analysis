8c6cfef30f924fdc8d5a066a6d54113bab0e9c3d
João Paulo Nogueira <joaonogueira@fisica.ufc.br>
2020-02-17 21:41:12 -0300

    kmeans modeling

---
+++ 05_modeling/01_kmeans_customer_segmentation.R
@@ -0,0 +1,133 @@
+# DS4B 101-R: R FOR BUSINESS ANALYSIS ----
+# CUSTOMER SEGMENTATION ----
+
+library(tidyverse)
+library(broom)
+library(umap)
+library(ggrepel)
+library(tidyquant)
+
+bike_orderlines_tbl <- read_rds("00_data/bike_sales/data_wrangled/bike_orderlines.rds")
+
+glimpse(bike_orderlines_tbl)
+
+
+# 1.0 CUSTOMER TRENDS ----
+# - GOAL: Mine Customer Purchase History for similarity to other "like" customers
+# - TECHNIQUES: K-Means Clustering, UMAP 2D Projection
+
+# 1.1 Get Customer Trends ----
+customer_trends_tbl <- bike_orderlines_tbl %>% 
+    
+    select(bikeshop_name, price, model, category_1, category_2, frame_material, quantity) %>% 
+    
+    # Summarization and group by
+    group_by(bikeshop_name, price, model, category_1, category_2, frame_material) %>% 
+    summarise(quantity_purchase = sum(quantity)) %>% 
+    ungroup() %>% 
+    
+    # Proportion
+    group_by(bikeshop_name) %>% 
+    mutate(prop_of_total = quantity_purchase / sum(quantity_purchase)) %>% 
+    ungroup()
+
+customer_trends_tbl
+
+
+# 1.2 Convert to User-Item Format (e.g. Customer-Product) ----
+
+customer_product_tbl <- customer_trends_tbl %>% 
+    select(bikeshop_name, model, prop_of_total) %>% 
+    spread(model, prop_of_total, fill = 0)
+
+customer_product_tbl
+
+
+# 2.0 MODELING: K-MEANS CLUSTERING ----
+
+# 2.1 Performing K-Means ----
+?kmeans
+
+kmeans_obj <- customer_product_tbl %>% 
+    select(-bikeshop_name) %>% 
+    kmeans(centers = 5, nstart = 100)
+
+kmeans_obj$centers
+
+kmeans_obj$cluster
+
+
+# 2.2 Tidying a K-Means Object ----
+
+# Returns the centers information for the kmeans model
+broom::tidy(kmeans_obj) %>% glimpse()
+
+# Returns overall summary metrics for the model
+broom::glance(kmeans_obj) 
+
+# Add column to the original data that was modeled with the predicted cluster
+broom::augment(kmeans_obj, customer_product_tbl) %>% 
+    select(bikeshop_name, .cluster)
+
+
+# 2.3 How many centers (customer groups) to use? ----
+
+# Function that works on 1 element
+kmeans_mapper <- function(centers = 3) {
+    customer_product_tbl %>% 
+        select(-bikeshop_name) %>% 
+        kmeans(centers = centers, nstart = 100)
+} 
+
+# Testing the function
+kmeans_mapper(3) %>% glance()
+
+# Mapping the funtion to many elements
+kmeans_mapped_tbl <- tibble(centers = 1:15) %>% 
+    mutate(k_means = centers %>% map(kmeans_mapper)) %>% 
+    mutate(glance  = k_means %>% map(glance))
+
+kmeans_mapped_tbl %>% 
+    unnest(glance) %>% 
+    select(centers, tot.withinss)
+
+
+# 2.4 Skree Plot ----
+
+kmeans_mapped_tbl %>% 
+    unnest(glance) %>% 
+    select(centers, tot.withinss) %>% 
+    
+    # Visualization
+    ggplot(aes(centers, tot.withinss)) +
+    geom_point(color = "#2c3e50", size = 4) +
+    geom_line(color = "#2c3e50", size = 1) +
+    ggrepel::geom_label_repel(aes(label = centers), color = "#2c3e50") +
+    
+    # Formatting
+    theme_tq() +
+    labs(
+        title = "Skree Plot",
+        subtitle = "Measures the distance each of the customer are from the closest K-Means center",
+        caption = "Based on the Skree Plot, we select 4 clusters to segment the customer base."
+    )
+
+
+# 3.0 VISUALIZATION: UMAP ----
+
+# 3.1 Use UMAP to get 2-D Projection ----
+
+
+# 3.2 Use K-Means to Add Cluster Assignments ----
+
+
+# 3.3 Visualize UMAP'ed Projections with Cluster Assignments ----
+
+
+
+
+# 4.0 ANALYZE PURCHASING TRENDS ----
+
+
+
+
